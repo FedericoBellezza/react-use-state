5ca851c22025b70b3f2d3acd4bccf4553adacf93
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-03 10:29:01 +0100

    fix bugs

--- src/components/Main/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function ({ data }) {
-  return (
-    <button onClick={() => open(data)} className="btn btn-primary m-3">
-      {data.title}
-    </button>
-  );
-}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { languages } from "../../assets/data/languages";
-import Button from "./Button/Button";
-import Accordion from "../Accordion/Accordion";
 
 export default function () {
+  let activeCategory = "HTML";
+  let activePost = languages.find(
+    (element) => element.title === activeCategory
+  );
+  const [changedPost, setPost] = useState(activePost);
+
   return (
     <main className="m-5">
       {languages.map((element) => {
-        return <Button data={element} />;
+        return (
+          <button
+            onClick={() => setPost(element)}
+            key={element.title}
+            className="btn btn-primary me-3"
+          >
+            {element.title}
+          </button>
+        );
       })}
-      {languages.map((element) => {
-        return <Accordion data={element} isOpen={false} />;
-      })}
+      <section>
+        <div className="card p-3 mt-3 ">
+          <h3>{changedPost.title}</h3>
+          <p>{changedPost.description}</p>
+        </div>
+      </section>
     </main>
   );
 }
